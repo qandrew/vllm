ba23cef3e8df2b732a86353659d3c01d47b913ba
chaunceyjiang <chaunceyjiang@gmail.com>
2025-10-24 15:02:34 +0000

    [Frontend] OpenAI Responses API supports Tool/Function calling
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- vllm/entrypoints/openai/serving_engine.py
+++ vllm/entrypoints/openai/serving_engine.py
@@ -1105,11 +1105,9 @@ class OpenAIServing:
                     "or Responses API requests."
                 )
                 raise NotImplementedError(msg)
-
-            if isinstance(request, ChatCompletionRequest):
-                request = tool_parser(tokenizer).adjust_request(  # type: ignore
-                    request=request
-                )
+            request = tool_parser(tokenizer).adjust_request(  # type: ignore
+                request=request
+            )
 
         if tokenizer is None:
             assert isinstance(request_prompt, str), (
