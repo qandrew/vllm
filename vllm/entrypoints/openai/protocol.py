af1965f493bda29916db994587f6f858856f62ed
Andrew Xia <axia@fb.com>
2025-11-03 10:20:26 -0800

    fix any type
    Signed-off-by: Andrew Xia <axia@fb.com>

--- vllm/entrypoints/openai/protocol.py
+++ vllm/entrypoints/openai/protocol.py
@@ -553,7 +553,7 @@ class ChatCompletionRequest(OpenAIBaseModel):
     stream_options: StreamOptions | None = None
     temperature: float | None = None
     top_p: float | None = None
-    tools: list[Any] | None = None
+    tools: list[ChatCompletionToolsParam] | None = None
     tool_choice: (
         Literal["none"]
         | Literal["auto"]
